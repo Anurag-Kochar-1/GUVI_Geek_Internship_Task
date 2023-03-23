69318a0cd956e4bb646f79b97717057409a33cf4
Anurag <anuragmarketing101@gmail.com>
2023-03-23 17:47:09 +0530

    Add: back-end initialized

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,19 @@
-console.log('ayo')
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.status(201).json("Home GET Request");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server connect to ${PORT}`);
+});
