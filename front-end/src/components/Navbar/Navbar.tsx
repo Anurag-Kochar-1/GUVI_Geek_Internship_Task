232d7a1be3229df7212d183fd33760a63117ef5d
Anurag <anuragmarketing101@gmail.com>
2023-03-24 09:50:45 +0530

    Redesign: Navbar

--- front-end/src/components/Navbar/Navbar.tsx
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    // const router = 
-
-    if(window?.location?.pathname === "/signup" || window?.location?.pathname === "/login") return null
+  if (
+    window?.location?.pathname === "/signup" ||
+    window?.location?.pathname === "/login"
+  )
+    return null;
 
   return (
-    <nav className="w-full h-20 px-5 md:px-5 py-2 relative flex justify-end items-center bg-light">
-      <div className="absolute w-full h-1 bg-gradient-to-r from-indigo-500 to-fuchsia-300 bottom-0 left-0 right-0"></div>
-      <Link to={`profile`}> Profile </Link>
-      {/* <button onClick={() => {
-        console.log(window.location)
-      }}> Log  </button> */}
+    <nav className="w-full h-20 px-5 md:px-5 py-2 relative flex justify-between items-center bg-light">
+      <Link to={`/`} className="flex justify-center items-center space-x-2">
+        <span className="w-10 h-10 rounded-full bg-black"></span>
+        <h1 className="font-medium text-lg text-black">Anurag Kochar </h1>
+      </Link>
+      <Link to={`profile`} className="font-medium text-lg text-black cursor-pointer"> Profile </Link>
+
+      <div className="absolute w-full h-2  bg-gradient-to-r from-indigo-500 to-fuchsia-300 bottom-0 left-0 right-0"></div>
     </nav>
   );
 };
