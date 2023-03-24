710d71d21b30664deb8ed86a43546c741d5156df
Anurag <anuragmarketing101@gmail.com>
2023-03-24 11:49:57 +0530

    Add: Suspense fallback screen

--- front-end/src/setup/routes-mangner/index.tsx
+++ front-end/src/setup/routes-mangner/index.tsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
 const SignUpPage = lazy(() => import("../../pages/SignUpPage/SignUpPage"));
@@ -8,7 +9,16 @@ const ProfilePage = lazy(() => import("../../pages/ProfilePage/ProfilePage"));
 
 const index = () => {
   return (
-    <Suspense fallback={<div>LOADING...</div>}>
+    <Suspense
+      fallback={
+        <div className="w-full h-screen bg-white flex justify-center items-center">
+          <AiOutlineLoading3Quarters
+            size={"1.2rem"}
+            className="text-brand animate-spin"
+          />
+        </div>
+      }
+    >
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
