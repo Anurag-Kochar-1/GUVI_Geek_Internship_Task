adc0a1a26a2f47c04149d9fa191fd32b1feed1a6
Anurag Kochar <93912807+Anurag-Kochar-1@users.noreply.github.com>
2023-05-28 21:30:25 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -7,7 +7,7 @@ const router = require("./routes/route");
 
 const app = express();
 
-app.use(express.json());
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
 app.use(
   cors({
     origin: ["http://localhost:3000/", "http://127.0.0.1:5173", "https://anurag-guvi-app.netlify.app"],
